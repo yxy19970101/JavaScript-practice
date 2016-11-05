a1fa8145e3f93c3a7d2a4303db5c924097242afb
yaoxueyan <3460535658@qq.com>
2016-11-05 16:45:49 +0800

    demo1.html demo1.css demo1.js

---
+++ demo1.js
@@ -0,0 +1,31 @@
+function seeIt1()
+{
+	var op31=document.getElementById('p31');
+
+	 op31.style.display='block';
+	 op31.style.color='black';
+}
+function seeIt2()
+{
+	var  op32=document.getElementById('p32');
+
+	op32.style.display='block';
+	op32.style.float='right';
+	op32n.style.color='black';
+}
+function seeIt3()
+{
+	var op33=document.getElementById('p33');
+
+	 op33.style.display='block';
+	 op33.style.float='right';
+	 op33.style.color='black';
+}
+function seeIt4()
+{
+	var  op34=document.getElementById('p34');
+
+	op34.style.display='block';
+	op34.style.float='right';
+	op34.style.color='black';
+}
